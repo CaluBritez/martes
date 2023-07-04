39c026437b01576d110966f66aeabeae67cf23c8
Lucas Britez <lucas_britez@hotmail.com>
2023-07-04 10:33:13 -0300

    codigo completo en archivo app

--- app.js
+++ app.js
@@ -10,21 +10,26 @@ require('ejs');
 
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // Middlewares
 // TODO: Implementar middlewares
-app.use(cors())
-app.use(helmet())
+app.use(cors());
+app.use(helmet());
 app.use(morgan('dev'))
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+})
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log('Server on port: '+port));
